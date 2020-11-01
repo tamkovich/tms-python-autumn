032a7ad54c4a52521cf9304b15c332d4f29d03b8
AntonSharypa <73784776+AntonSharypa@users.noreply.github.com>
2020-11-01 17:49:03 +0300

    Add files via upload

---
+++ task_1_1.py
@@ -0,0 +1,18 @@
+
+print("Привет, сметрный. Время поиграть")
+print("Введи число a")
+a=float(input())
+print("Введи число b")
+b=float(input())
+print("Вот ответы на твои вопросы")
+print("Сумма")
+print(a+b)
+print("Разность a-b")
+print(a-b)
+print("Разность b-a")
+print(b-a)
+print("Произведение")
+print(a*b)
+input()
+
+
