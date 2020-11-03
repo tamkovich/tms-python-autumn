904e43609100f6af55be747648c6778319ad99c3
Stanislav Kravchenko <73652742+Obre4en@users.noreply.github.com>
2020-11-04 00:21:54 +0300

    Add files via upload

---
+++ src/task_2_1.py
@@ -0,0 +1,7 @@
+a = int(input())
+b = a / 1000
+c = str(b)
+if c.endswith('0'):
+    print('millennium')
+else:
+    print(' ')
---
+++ src/task_2_2.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+if n < 50:
+    if n >= 20 and n <= 50:
+        print('кафе')
+
+    elif n < 20:
+        print('дом')
+else:
+    print('ресторан')
