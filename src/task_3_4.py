be2cfd2456a4400c6ac3b0cdda598c39e20d9617
Igor Chvyrov <igorchvyrov@MacBook-Pro-Igor.local>
2020-11-03 22:34:43 +0300

    remake task_3_4 HM3: Igor Chvyrov

--- src/task_3_4.py
+++ src/task_3_4.py
@@ -1,9 +1,12 @@
-string = input("input nechet str ")
+string = input("input  str ")
 print(f"length of string =  {len(string)}")
-index =int(len(string)/2)
+index = 0
+if int(len(string)%2) == 0:
+    index = int(len(string)/2)-1
+else:
+    index =int(len(string)/2)
 center_letter = string[index]
-print(f" center letter {center_letter}")
-print()
+print(f"center letter {center_letter}")
 if center_letter == string[0] :
     new_string = string[1:-1]
-    print(new_string)
+    print(f"result {new_string}")
