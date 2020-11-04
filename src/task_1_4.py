156801d300cc16a911f1447b7ea08aa860aafe2b
Maxim <36435365+MaximBar1@users.noreply.github.com>
2020-11-04 16:18:05 +0300

    HW1: Maxim Baranov

---
+++ src/task_1_4.py
@@ -0,0 +1,4 @@
+a = float(input("first number:"))
+b = float(input("second number:"))
+print('average = ',(a+b)/2)
+print('geomrtric = ',(a*b)**0.5)
---
+++ src/task_1_5.py
@@ -0,0 +1,4 @@
+a = float(input('first leg: '))
+b = float(input('second leg: '))
+print("hypotenuse:", (a**2+b**2)**(1/2))
+print("area:", a*b/2)
