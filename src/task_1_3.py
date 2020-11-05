4933a9ef9df23e760f994eee25535dc1f35f9cfd
Rekets Alexander <73691955+Rekets@users.noreply.github.com>
2020-11-05 15:30:31 +0300

    add HW1 in new branch

---
+++ src/task_1_3.py
@@ -0,0 +1,7 @@
+length = float(input('Cube edge length = '))
+
+volume = length ** 2 * length
+area = length ** 2
+
+print('Volume = ', volume)
+print('Area = ', area)
---
+++ src/task_1_5.py
@@ -0,0 +1,10 @@
+a = float(input('First leg: '))
+b = float(input('Second leg: '))
+
+# Найти его гипотенузу:
+hypotenuse = pow((a ** 2 + b ** 2),(1 / 2))
+print('hypotenuse: ', hypotenuse)
+
+# Площадь:
+area = a * b * (1 / 2)
+print('area: ', area)
