033f546c0495a17bd5e843fae4b37de76810701c
ZubritskiyAlex <ZubritskiyAlexander@gmail.com>
2020-11-06 22:35:18 +0300

    HW_4: ZubritskiyAlex

---
+++ src/task 4_2.py
@@ -0,0 +1,17 @@
+#Дан список целых чисел. Подсчитать сколько четных чисел в списке
+import random
+
+length_of_list = int(input("Enter the length of list "))
+first_list = random.sample(range(100), length_of_list)
+print("first list:  \n ", first_list)
+even = len([i for i in first_list if i % 2 == 0])
+print("number of even elements: \n",even)
+even = 0
+
+i = 0
+while first_list:
+    x = first_list.pop()
+    if x % 2 == 0:
+        even += 1
+    i += 1
+print("number of even elements from while : \n",even)
