507a800c0c9faacab091789efa61408b9c2b859f
Alexander <55025428+ZubritskiyAlex@users.noreply.github.com>
2020-11-06 22:15:08 +0300

    Revert "HW3: ZubritskiyAlex"

--- src/task_3_3.py
+++
@@ -1,8 +0,0 @@
-st = input("Enter the number ")
-a = len(st)
-if a > 10:
-    print(st+'!!!')
-elif a < 10:
-    print(st[1])
-else:
-    print("длина равна 10 символов")
--- src/task_3_4.py
+++
@@ -1,9 +0,0 @@
-stroka = input("Enter the str")
-a = len(stroka)
-middle = int(a / 2)
-if a % 2 == 0:
-    print(stroka[middle-1])
-elif stroka[0] == stroka[middle]:
-    print(stroka[1:a-1])
-else:
-    print(stroka[middle])
