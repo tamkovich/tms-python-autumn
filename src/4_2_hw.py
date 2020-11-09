85ede0d7d14716665072a7bf611c4d4992f56c82
Yury Rybianets <rybianets.y@gmail.com>
2020-11-09 19:00:36 +0300

    ДЗ 4. 4_3 - есть вопросик по решению через while.

---
+++ src/4_2_hw.py
@@ -0,0 +1,12 @@
+# list = [14, 7, 13, 132, 6, 98, 93, 1, 4, 5, 7]
+# even = 0
+# odd = 0
+# for i in range(len(list)):
+#     while i > 0:
+#         if i % 2 == 0:
+#             even += 1
+#         else:
+#             odd += 1
+#         i = i // 10
+# print('even', even)
+# print('odd', odd)
