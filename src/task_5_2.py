6a456cb6c45cacc4b092c59114fb46714de52a51
Oneleeshot <73745229+Oneleeshot@users.noreply.github.com>
2020-11-29 19:55:08 +0300

    Update task_5_2.py

--- src/task_5_2.py
+++ src/task_5_2.py
@@ -1,15 +1,19 @@
-number = 123
-result = []
-summa = 0
-proiz = 1
-while True:
-    if number > 0:
-        x = number % 10
-        result.append(x)
-        number //= 10
-    else:
-        break
-for i in range(3):
-    summa += int(result[i])
-    proiz *= int(result[i])
-print(summa, proiz)
+number = input('Введите число: ')
+if number.isdigit() == True:
+    number = int(number)
+    result = []
+    summa = 0
+    proiz = 1
+    while True:
+        if number > 0:
+            x = number % 10
+            result.append(x)
+            number //= 10
+        else:
+            break
+    for i in range(3):
+        summa += int(result[i])
+        proiz *= int(result[i])
+    print(summa, proiz)
+else:
+    print('Введен не верный тип данных')
