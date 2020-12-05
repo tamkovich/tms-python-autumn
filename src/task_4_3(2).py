ca67701d12c6e88ba13a5ac88033cf5094da50e1
Oneleeshot <73745229+Oneleeshot@users.noreply.github.com>
2020-12-05 15:36:04 +0300

    Update task_4_3(2).py

--- src/task_4_3(2).py
+++ src/task_4_3(2).py
@@ -1,3 +1,8 @@
+'''Дан словарь: {'test': 'test_value', 'europe': 'eur', 'dollar': 'usd', 'ruble': 'rub'}
+Добавить каждому ключу число равное длине этого ключа (пример {‘key’: ‘value’} -> {‘key3’:
+‘value’}). Чтобы получить список ключей - использовать метод .keys()'''
+
+
 a = {'test': 'test_value', 'europe': 'eur', 'dollar': 'usd', 'ruble': 'rub'}
 b = list(a.keys())
 f = a.values()
