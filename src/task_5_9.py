1c5a5cc7b10d4423ff02badd6cd4cc45ad9dad72
Oneleeshot <73745229+Oneleeshot@users.noreply.github.com>
2020-12-07 18:40:40 +0300

    Update task_5_9.py

--- src/task_5_9.py
+++ src/task_5_9.py
@@ -1,12 +1,15 @@
-m = int(input())
-n = int(input())
-x = list(range(m, n+1))
-lx = len(x)
-matrix = []
-for number in range(0, lx):
-    y = []
-    for delitel in range(2, n):
-        if x[number] % delitel == 0 and x[number] != delitel:
-            y.append(delitel)
-    matrix.append(y)
-print(matrix)
+m = input()
+n = input()
+if m.isdigit() == True and n.isdigit() == True:
+    m = int(m)
+    n = int(n)
+    x = list(range(m, n+1))
+    lx = len(x)
+    matrix = []
+    for number in range(0, lx):
+        y = []
+        for delitel in range(2, n):
+            if x[number] % delitel == 0 and x[number] != delitel:
+                y.append(delitel)
+        matrix.append(y)
+    print(matrix)
