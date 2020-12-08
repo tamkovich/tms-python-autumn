95c1be15c16a28e709f2dc55ca7a529d821f27d7
Oneleeshot <73745229+Oneleeshot@users.noreply.github.com>
2020-12-08 20:08:12 +0300

    Update task_9_2.py

--- src/task_9_2.py
+++ src/task_9_2.py
@@ -2,5 +2,6 @@
 # количество именных аргументов и выводит словарь с ключами удвоенной
 # длины. {‘abc’: 5} -> {‘abcabc’: 5}
 
-x = (lambda **kwargs: {key * 2: value for key, value in kwargs.items()})(abc=5, trd=7)
+x = (lambda **kwargs: {key * 2: value for key, value in kwargs.items()})\
+    (abc=5, trd=7)
 print(x)
