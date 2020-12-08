cef0b0b0c0ec70971d56cc0f3c65ffe194714496
Oneleeshot <73745229+Oneleeshot@users.noreply.github.com>
2020-12-08 19:39:39 +0300

    Update task_8_1.py

--- src/task_8_1.py
+++ src/task_8_1.py
@@ -6,7 +6,7 @@
 def fact2(n: int):
     fact1 = 1
     fact2 = 2
-    for i in range(1, n-1):
+    for i in range(1, n - 1):
         if n % 2 == 0 and i % 2 == 0:
             fact2 *= i + 2
         elif n % 2 != 0 and i % 2 != 0:
