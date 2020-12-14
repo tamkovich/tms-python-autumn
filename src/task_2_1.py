75ca612c07979487fe250c3739be54176a41d08a
Anton Sharypa <antonsharypa@tut.by>
2020-12-14 20:17:59 +0300

    HW2 : Anton Sharypa

---
+++ src/task_2_1.py
@@ -0,0 +1,8 @@
+"""Создать строку равную третьему символу введенной строки."""
+
+print("Vvedite stroku")
+b = input()
+print("kakoi simvol vas interesuet")
+a = int(input())
+c = a - 1
+print(b[c])
